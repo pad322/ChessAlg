182723055f64537851d18e280471e5cbd5c7e941
pad322 <79305213+pad322@users.noreply.github.com>
2023-05-16 22:46:20 +0300

    v.0.3

--- ChessApp/ChessApp/ChessApp.cpp
+++ ChessApp/ChessApp/ChessApp.cpp
@@ -3,19 +3,21 @@
 #include <chrono>
 #include <Windows.h>
 #include <future>
-
-using namespace std;
+#include <fstream>
 
 char btn;
 bool run_mode = true;
 bool lan = true;
+bool intro = true;
 int interval = 500;
 
+/*
 void timer(chrono::seconds delay)
 {
 	this_thread::sleep_for(delay);
-	cout << "\n\tPress a number on your keyboard to navigate the menu!\n";
+	std::cout << "\n\tPress a number on your keyboard to navigate the menu!\n";
 }
+*/
 
 void chess_menu() 
 {
@@ -23,19 +25,19 @@ void chess_menu()
 
 	if (lan)
 	{
-		cout << "\n\tSelect a chess problem!";
-		cout << "\n\n\t1. Queen problem with minimal nr. of pieces";
-		cout << "\n\t2. Queens dominating";
-		cout << "\n\t3. Queen on horseback";
-		cout << "\n\t4. Independent rooks";
+		std::cout << "\n\tSelect a chess problem!";
+		std::cout << "\n\n\t1. Queen problem with minimal nr. of pieces";
+		std::cout << "\n\t2. Queens dominating";
+		std::cout << "\n\t3. Queen on horseback";
+		std::cout << "\n\t4. Independent rooks";
 	}
 	else
 	{
-		cout << "\n\tValassza ki a sakk problemat";
-		cout << "\n\n\t1. Kiralyno problema legkevesebb darabbal";
-		cout << "\n\t2. Kiralyno lefedes";
-		cout << "\n\t3. Kiralyno lohaton lefedes";
-		cout << "\n\t4. Kulonallo bastyak";
+		std::cout << "\n\tValassza ki a sakk problemat";
+		std::cout << "\n\n\t1. Kiralyno problema legkevesebb darabbal";
+		std::cout << "\n\t2. Kiralyno lefedes";
+		std::cout << "\n\t3. Kiralyno lohaton lefedes";
+		std::cout << "\n\t4. Kulonallo bastyak";
 	}
 
 	btn = _getch();
@@ -46,17 +48,17 @@ void chess_menu()
 
 	if (lan)
 	{
-		cout << "\n\tSelect a programming approach!";
-		cout << "\n\n\t1. Dynamic";
-		cout << "\n\t2. Greedy";
-		cout << "\n\t3. Backtracking";
+		std::cout << "\n\tSelect a programming approach!";
+		std::cout << "\n\n\t1. Dynamic";
+		std::cout << "\n\t2. Greedy";
+		std::cout << "\n\t3. Backtracking";
 	}
 	else
 	{
-		cout << "\n\tValassza ki a programozasi modszert!";
-		cout << "\n\n\t1. Dinamikus";
-		cout << "\n\t2. Greedy";
-		cout << "\n\t3. Backtracking";
+		std::cout << "\n\tValassza ki a programozasi modszert!";
+		std::cout << "\n\n\t1. Dinamikus";
+		std::cout << "\n\t2. Greedy";
+		std::cout << "\n\t3. Backtracking";
 	}
 
 	btn = _getch();
@@ -65,55 +67,80 @@ void chess_menu()
 
 void settings_menu()
 {
+	std::ofstream out("data.txt", std::ofstream::trunc);
+
 Settings:
 	system("cls");
 
 	if (lan)
-		cout << "\n\t1. Change Running mode - ";
+		std::cout << "\n\t1. Change Running mode - ";
 	else
-		cout << "\n\t1. Futasi mod valtoztatasa - ";
+		std::cout << "\n\t1. Futasi mod valtoztatasa - ";
 
 	if (run_mode)
 	{
 		if (lan)
-			cout << "Step-by-step iteration\n";
+			std::cout << "Step-by-step iteration\n";
 		else
-			cout << "Lepesrol-lepesre iteralva\n";
+			std::cout << "Lepesrol-lepesre iteralva\n";
 	}
 	else
 	{
 		if (lan)
-			cout << "Pausing mode\n";
+			std::cout << "Pausing mode\n";
 		else
-			cout << "Szunetelheto mod\n";
+			std::cout << "Szunetelheto mod\n";
 	}
 
 	if (lan)
-		cout << "\t2. Step interval - ";
+		std::cout << "\t2. Step interval - ";
 	else
-		cout << "\t2. Lepesenkenti idotartam - ";
+		std::cout << "\t2. Lepesenkenti idotartam - ";
 
-	cout << interval << '\n';
+	std::cout << interval << '\n';
 
 
 	if (lan)
-		cout << "\t3. Language - ";
+		std::cout << "\t3. Language - ";
 	else
-		cout << "\t3. Nyelv - ";
+		std::cout << "\t3. Nyelv - ";
 
 	if (lan)
 	{
-		cout << "English\n";
+		std::cout << "English\n";
 	}
 	else
 	{
-		cout << "Magyar\n";
+		std::cout << "Magyar\n";
 	}
 
 	if (lan)
-		cout << "\t4. Back to Main Menu\n";
+	{
+		std::cout << "\t4. Skip intro - ";
+
+		if (intro)
+		{
+			std::cout<< "Disabled\n";
+		}
+		else
+			std::cout << "Enabled\n";
+	}
 	else
-		cout << "\t4. Vissza a Fo Menube\n";
+	{
+		std::cout << "\t4. Bevezeto atugrasa - ";
+
+		if (intro)
+		{
+			std::cout << "Kikapcsolva\n";
+		}
+		else
+			std::cout << "Bekapcsolva\n";
+	}
+
+	if (lan)
+		std::cout << "\t5. Back to Main Menu\n";
+	else
+		std::cout << "\t5. Vissza a Fo Menube\n";
 
 	btn = _getch();
 
@@ -134,6 +161,10 @@ Settings:
 		goto Settings;
 		break;
 	case '4':
+		intro = !intro;
+		goto Settings;
+		break;
+	case '5':
 		break;
 	default :
 		goto Settings;
@@ -146,23 +177,23 @@ void notes_menu()
 	system("cls");
 	if(lan)
 	{
-		cout << "\n\t\t Chess App\n";
-		cout << "\t Created by Torok Csongor\n";
-		cout << "\n\tThe app is meant for displaying and solving different mathematical chess problems\n\tcomparing these in terms of time and optimization.\n";
-		cout << "\n\tStep-by-step mode : Each time Space is pressed, the algorithm advances one step.";
-		cout << "\n\tPausing mode : Each time Space is pressed, the algorithm stops.";
-		cout << "\n\tIn the Pausing mode, the algorithm advances in the given intervals value, representing milliseconds between each step.";
-		cout << "\n\n\tPress any button to return to the Main menu!";
+		std::cout << "\n\t\t Chess:Alg\n";
+		std::cout << "\t Created by Torok Csongor\n";
+		std::cout << "\n\tThe app is meant for displaying and solving different mathematical chess problems\n\tcomparing these in terms of time and optimization.\n";
+		std::cout << "\n\tStep-by-step mode : Each time Space is pressed, the algorithm advances one step.";
+		std::cout << "\n\tPausing mode : Each time Space is pressed, the algorithm stops.";
+		std::cout << "\n\tIn the Pausing mode, the algorithm advances in the given intervals value, representing milliseconds between each step.";
+		std::cout << "\n\n\tPress any button to return to the Main menu!";
 	}
 	else
 	{
-		cout << "\n\t\t Sakk Alkalmazas\n";
-		cout << "\t     Keszitette Torok Csongor\n";
-		cout << "\n\tAz alkalmazast kulonbozo sakkos matematikai problemak megjelenitesere es megoldasara keszult\n\tilletve ezek osszehasonlitasara ido es optimalizalas szempontjabol.\n";
-		cout << "\n\tLepesrol-lepesre iteralas : Minden alkalommal amikor megnyomja a Space-t, egy lepest tesz az algoritmus.";
-		cout << "\n\tSzunetelo mod : Minden Space gomb lenyomasakor, szunetel az algoritmus futasa.";
-		cout << "\n\tEbben a modban az algoritmus az allithato idotartam erteke szerint lepked, miliszekundumokban szamolva.";
-		cout << "\n\n\tNyomjon meg barmilyen gombot a Fo Menuhoz valo visszatereshez!";
+		std::cout << "\n\t\t Chess:Alg\n";
+		std::cout << "\t     Keszitette Torok Csongor\n";
+		std::cout << "\n\tAz alkalmazast kulonbozo sakkos matematikai problemak megjelenitesere es megoldasara keszult\n\tilletve ezek osszehasonlitasara ido es optimalizalas szempontjabol.\n";
+		std::cout << "\n\tLepesrol-lepesre iteralas : Minden alkalommal amikor megnyomja a Space-t, egy lepest tesz az algoritmus.";
+		std::cout << "\n\tSzunetelo mod : Minden Space gomb lenyomasakor, szunetel az algoritmus futasa.";
+		std::cout << "\n\tEbben a modban az algoritmus az allithato idotartam erteke szerint lepked, miliszekundumokban szamolva.";
+		std::cout << "\n\n\tNyomjon meg barmilyen gombot a Fo Menuhoz valo visszatereshez!";
 	}
 
 	btn = _getch();
@@ -172,8 +203,64 @@ void exit_part()
 {
 }
 
+void intro_screen()
+{
+	Sleep(500);
+
+	std::cout << '\n';
+	std::cout << "    _:_\n";
+	Sleep(40);
+	std::cout << "   '-.-'\n";
+	Sleep(40);
+	std::cout << "  __.'.__       ()   \n";
+	Sleep(40);
+	std::cout << " |_______|   .-:--:-.\n";
+	Sleep(40);
+	std::cout << "  \\=====/     \\____/                                                              ()\n";
+	Sleep(40);
+	std::cout << "   )___(      {====}     _____  _                                   _             /\\  \n";
+	Sleep(40);
+	std::cout << "  /_____\\      )__(     / ____|| |                     _     /\\    | |           /  \\       (\=,  \n";
+	Sleep(40);
+	std::cout << "   |   |      /____\\   | |     | |__    ___  ___  ___ (_)   /  \\   | |  __ _    (    )    //  .\\ \n";
+	Sleep(40);
+	std::cout << "   |   |       |  |    | |     | '_ \\  / _ \\/ __|/ __|     / /\\ \\  | | / _` |    )__(    (( \\_  \\\n";
+	Sleep(40);
+	std::cout << "   |   |       |  |    | |____ | | | ||  __/\\__ \\__  \\ _  / ____ \\ | || (_| |   /____\\    ))  `\\_) \n";
+	Sleep(40);
+	std::cout << "   |   |       |  |     \\_____||_| |_| \\___||___/|___/(_)/_/    \\_\\|_| \\__, |    |  |    (/     \\ \n";
+	Sleep(40);
+	std::cout << "   |   |       |  |                                                     __/ |    |  |     | _.-'| \n";
+	Sleep(40);
+	std::cout << "  /_____\\     /____\\                                                   |___/    /____\\     )___( \n";
+	Sleep(40);
+	std::cout << " (=======)   (======)                                                          (======)   (=====)\n";
+	Sleep(40);
+	std::cout << " }======={   }======{                                                          }======{   }====={\n";
+	Sleep(40);
+	std::cout << "(_________) (________)                                                        (________) (_______)\n";
+
+	Sleep(1000);
+	if (lan)
+	{
+		std::cout << "\n\t\t\t\t   Press Any Button to Continue!";
+	}
+	else
+	{
+		std::cout << "\n\tNyomjon meg barmilyen gombot a tovabblepeshez!";
+	}
+
+	btn = _getch();
+}
+
 int main()
 {
+	do
+	{
+		if (intro)
+			intro_screen();
+	} while (0);
+
 MainMenu :
 
 	system("cls");
@@ -184,19 +271,19 @@ MainMenu :
 
 	if(lan)
 	{
-		cout << "\n\t\tMain Menu\n\n";
-		cout << "\t1. Chess Problems\n";
-		cout << "\t2. Settings\n";
-		cout << "\t3. Notes\n";
-		cout << "\t4. Exit\n";
+		std::cout << "\n\t\tMain Menu\n\n";
+		std::cout << "\t1. Chess Problems\n";
+		std::cout << "\t2. Settings\n";
+		std::cout << "\t3. Notes\n";
+		std::cout << "\t4. Exit\n";
 	}
 	else
 	{
-		cout << "\n\t\tFo menu\n\n";
-		cout << "\t1. Sakk problemak\n";
-		cout << "\t2. Beallitasok\n";
-		cout << "\t3. Jegyzetek\n";
-		cout << "\t4. Kilepes\n";
+		std::cout << "\n\t\tFo menu\n\n";
+		std::cout << "\t1. Sakk problemak\n";
+		std::cout << "\t2. Beallitasok\n";
+		std::cout << "\t3. Jegyzetek\n";
+		std::cout << "\t4. Kilepes\n";
 	}
 
 	btn = _getch();
@@ -213,18 +300,20 @@ MainMenu :
 		notes_menu();
 		break;
 	case '4':
-		
+
+		Exit :
+
 		system("cls");
 
 		if (lan)
 		{
-			cout << "\n\tAre you sure you want to quit?\n";
-			cout << "\t1. Yes\t2. No";
+			std::cout << "\n\tAre you sure you want to quit?\n";
+			std::cout << "\t1. Yes\t2. No";
 		}
 		else
 		{
-			cout << "\n\tBiztos ki szeretne lepni?\n";
-			cout << "\t1. Igen\t2. Nem";
+			std::cout << "\n\tBiztos ki szeretne lepni?\n";
+			std::cout << "\t1. Igen\t2. Nem";
 		}
 
 		btn = _getch();
@@ -238,6 +327,8 @@ MainMenu :
 		case '2':
 			goto MainMenu;
 			break;
+		default :
+			goto Exit;
 		}
 
 		break;
